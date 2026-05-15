1471ab383fc272e0b6d4d4a067109428acad4df3
Paramveer R <paramveerpc2211@gmail.com>
2026-05-15 12:31:24 +0530

    init

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+function greet(name: string): string  {
+    return `hello ${name}`
+ }
+ console.log(greet("world"))
+
+
+ // even u can tyoecast the variable
+ const username : string = "param"
+ console.log(greet(username))
